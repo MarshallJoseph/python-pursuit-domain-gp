39c81e90f494acecc5706670e1705bd80b08f546
MarshallJoseph <mrmarshalljoseph@gmail.com>
2022-10-25 22:50:11 -0400

    minor changes

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from deap import gp
 
 from array import *
 
-
+# TODO: Store moves/actions that predator takes
 # TODO: Create sensing function for predator
 # TODO: Create speed update function for predator and test their functionality
 # TODO: Add terminals/operators for GP tree
@@ -35,18 +35,21 @@ def prog3(out1, out2, out3):
 
 class PreyAgent:
 
-    def __init__(self, rows, cols):
-        self.x_pos = random.random() * (rows - 1)  # x coordinate of prey in 2-d space
-        self.y_pos = random.random() * (cols - 1)  # y coordinate of prey in 2-d space
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.x_pos = random.random() * (width - 1)  # x coordinate of prey in 2-d space
+        self.y_pos = random.random() * (height - 1)  # y coordinate of prey in 2-d space
         self.x_rot = random.random() * 2 - 1  # x rotation of prey in 2-d space [-1, 1]
         self.y_rot = random.random() * 2 - 1  # y rotation of prey in 2-d space [-1, 1]
         self.speed = 0.7  # speed of prey
 
     def move_forward(self):
-        next_x_pos = self.x_pos + (self.x_rot * self.speed)  # calculate next x coordinate
-        next_y_pos = self.y_pos + (self.y_rot * self.speed)  # calculate next y coordinate
-        self.x_pos = next_x_pos  # set prey to new x coordinate
-        self.y_pos = next_y_pos  # set prey to new y coordinate
+        new_x_pos = self.x_pos + (self.x_rot * self.speed)  # calculate next x coordinate
+        new_y_pos = self.y_pos + (self.y_rot * self.speed)  # calculate next y coordinate
+        if 0 < new_x_pos < self.width and 0 < new_y_pos < self.height:
+            self.x_pos = new_x_pos  # set prey to new x coordinate
+            self.y_pos = new_y_pos  # set prey to new y coordinate
         self.rotate()  # rotate in random direction before taking step forward
 
     def rotate(self):
@@ -58,8 +61,8 @@ class PredPreySimulator:
 
     def __init__(self, max_steps):
         # * Environment Properties *
-        self.rows = 21  # number of rows in discrete environment overlay, default 201
-        self.cols = 21  # number of columns in discrete environment overlay, default 201
+        self.width = 200  # number of rows in discrete environment overlay, default 201
+        self.height = 200  # number of columns in discrete environment overlay, default 201
         self.num_prey = 8  # number of prey, default 15
         # * Simulation Properties *
         self.max_moves = max_steps  # limit of steps per pred/prey per simulation loop
@@ -67,14 +70,14 @@ class PredPreySimulator:
         self.captured = 0  # number of prey captured by predator
         self.routine = None
         # * Initialize Predator *
-        self.x_pos = self.rows / 2  # x coordinate of predator in 2-d space
-        self.y_pos = self.cols / 2  # y coordinate of predator in 2-d space
+        self.x_pos = self.width / 2  # x coordinate of predator in 2-d space
+        self.y_pos = self.height / 2  # y coordinate of predator in 2-d space
         self.x_rot = random.random() * 2 - 1  # x rotation of predator in 2-d space [-1, 1]
         self.y_rot = random.random() * 2 - 1  # y rotation of predator in 2-d space [-1, 1]
         self.speed = 1  # speed of predator
         self.max_speed = 1  # max speed of predator
         # * Initialize Prey *
-        self.prey = [PreyAgent(self.rows, self.cols) for _ in range(self.num_prey)]
+        self.prey = [PreyAgent(self.width, self.height) for _ in range(self.num_prey)]
         self.print_pred_properties()
         self.print_prey_properties()
 
@@ -98,16 +101,14 @@ class PredPreySimulator:
     def move_forward(self):
         if self.moves < self.max_moves:
             self.moves += 1  # increase number of moves by 1
-            next_x_pos = self.x_pos + (self.x_rot * self.speed)  # calculate next x coordinate
-            next_y_pos = self.y_pos + (self.y_rot * self.speed)  # calculate next y coordinate
-            self.x_pos = next_x_pos  # set pred to new x coordinate
-            self.y_pos = next_y_pos  # set pred to new y coordinate
+            new_x_pos = self.x_pos + (self.x_rot * self.speed)  # calculate next x coordinate
+            new_y_pos = self.y_pos + (self.y_rot * self.speed)  # calculate next y coordinate
+            if 0 < new_x_pos < self.width and 0 < new_y_pos < self.height:
+                self.x_pos = new_x_pos  # set pred to new x coordinate
+                self.y_pos = new_y_pos  # set pred to new y coordinate
             # * Prey Movement *
             for i, p in enumerate(self.prey):
                 p.move_forward()  # move the prey randomly
-            print("*** *** *** *** *** *** *** *** *** ***")
-            self.print_pred_properties()
-            self.print_prey_properties()
 
     def rotate(self, new_x_rot, new_y_rot):
         # check conditions of new x rotation
@@ -133,11 +134,13 @@ class PredPreySimulator:
 # Initialize simulation with 5000 steps
 sim = PredPreySimulator(5000)
 sim.move_forward()
+print("*** *** *** *** *** *** *** *** *** ***")
+sim.print_pred_properties()
+sim.print_prey_properties()
 
 
 def main():
     random.seed(1)
-
 
 
 if __name__ == "__main__":
